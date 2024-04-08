e0975d7f06dd1440ffb5b63ab1c6bce8581e9d79
adaneverton <adaneverton@outlook.com>
2024-04-08 14:26:07 +0100

    Desafio do Heroi

---
+++ index.js
@@ -0,0 +1,10 @@
+// Solicitação do nome do herói
+var nomeHeroi = prompt("Spectron")
+
+// Solicitação da quantidade de XP do herói
+var xpHeroi = parseInt(prompt("Digite a quantidade de XP do herói: "))
+
+// Variável para armazenar o nível do herói
+var nivelHeroi
+
+
